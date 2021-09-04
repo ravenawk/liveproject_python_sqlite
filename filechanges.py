aef9945844f6fa2bb1ef0dbfa87b279e8d98b22c
William Martin <wpmartin@gmail.com>
2021-09-04 14:59:18 -0700

    Worked out more of the create and query

--- filechanges.py
+++ filechanges.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python3
 
 import sqlite3
+import sys
+import os
 
 def create_database(dbname):
-    conn = sqlite3.connect(dbname)
+    conn = None
+    try:
+        conn = sqlite3.connect(dbname)
+        print(f"Connection is established: {dbname}")
+    except sqlite3.Error:
+        print(sqlite3.Error)
+    finally:
+        conn.close()
 
+def query_database(dbname):
+    
 
 if __name__ == "__main__":
     dbname = 'file_changes.db'
-    conn = sqlite3.connect(dbname)
-
+    create_database(dbname)
+    query_database(dbname)
